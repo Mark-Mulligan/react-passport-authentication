5eb37a685d3953a38e442f6e74f554961b54ab41
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-11 16:56:05 -0500

    creating server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const passportLocal = require('passport-local').Strategy;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  sercret: process.env.SECRET,
+  resave: true
+}))
+
+app.listen(4000, () => {
+  console.log('Server has started')
+})
