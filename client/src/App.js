1de4c28ef6977401aed792a0e7aed1ae0204d397
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-12 21:43:56 -0500

    login/register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginForm, setLoginForm] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (loginForm) {
+      console.log("login: " + username, password);
+    } else {
+      console.log("register: " + username, password);
+    }
+  };
+
+  const handleFormSwitch = () => {
+    setLoginForm(!loginForm);
+    setUsername('');
+    setPassword('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>{loginForm ? "Login" : "Register"}</h1>
+        <div>
+          <label>
+            Username
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="username">Password</label>
+          <input
+            name="username"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit">{loginForm ? "Login" : "Register"}</button>
+        <button type="button" onClick={handleFormSwitch}>{loginForm ? "Sign up?" : "Go to Login"}</button>
+      </form>
     </div>
   );
 }
